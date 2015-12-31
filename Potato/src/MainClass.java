1de6b7393402ec8b7f29e90c8256fd2df74fb90a
Samuel Croteau <SCroteau94@gmail.com>
2015-12-31 00:33:11 -0500

    better sysout

--- Potato/src/MainClass.java
+++ Potato/src/MainClass.java
@@ -10,7 +10,7 @@ public class MainClass {
 	public static void main(String[] args) {
 		System.out.println("yo bob");
 		
-		System.out.println("thats cool!");
+		System.out.println("thats super cool!");
 		
 		FramePrincipale g = new FramePrincipale();
 		Thread t = new Thread(g);
