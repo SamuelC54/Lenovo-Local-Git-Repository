74e2dbffc55f44c299622549b4bef0bf50d36d1f
Samuel Croteau <SCroteau94@gmail.com>
2015-12-31 00:30:40 -0500

    change sysout

--- Potato/src/MainClass.java
+++ Potato/src/MainClass.java
@@ -8,7 +8,9 @@
 public class MainClass {
 
 	public static void main(String[] args) {
-		System.out.println("yo");
+		System.out.println("yo bob");
+		
+		System.out.println("thats cool!");
 		
 		FramePrincipale g = new FramePrincipale();
 		Thread t = new Thread(g);
